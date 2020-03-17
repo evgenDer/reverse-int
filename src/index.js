98d8f675fab5d06e9ae097255811d451d2e8ad4a
Derevygo Evgeniya <evgeniya.nice@yandex.by>
2020-03-17 15:38:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+    let result=(n>0) ? Number(String(n).split("").reverse().join("")) : Number(String(-n).split("").reverse().join(""));
+    return result;
 }
